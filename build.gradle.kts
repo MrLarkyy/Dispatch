dbbfc23bbe034a1b4710bb49b529ac425817e388
MrLarkyy_ <studiotadikl@gmail.com>
2026-01-22 10:44:45 +0100

    Update build configuration for Kotlin, Maven publishing, and environment setup

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,11 @@
 plugins {
-    kotlin("jvm") version "2.2.21"
+    kotlin("jvm") version "2.3.0"
+    id("co.uzzu.dotenv.gradle") version "4.0.0"
+    `maven-publish`
 }
 
 group = "gg.aquatic.dispatch"
-version = "1.0-SNAPSHOT"
+version = "26.0.1"
 
 repositories {
     mavenCentral()
@@ -21,4 +23,32 @@ kotlin {
 
 tasks.test {
     useJUnitPlatform()
+}
+
+val maven_username = if (env.isPresent("MAVEN_USERNAME")) env.fetch("MAVEN_USERNAME") else ""
+val maven_password = if (env.isPresent("MAVEN_PASSWORD")) env.fetch("MAVEN_PASSWORD") else ""
+
+publishing {
+    repositories {
+        maven {
+            name = "aquaticRepository"
+            url = uri("https://repo.nekroplex.com/releases")
+
+            credentials {
+                username = maven_username
+                password = maven_password
+            }
+            authentication {
+                create<BasicAuthentication>("basic")
+            }
+        }
+    }
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "gg.aquatic"
+            artifactId = "Blokk"
+            version = "${project.version}"
+            from(components["java"])
+        }
+    }
 }
