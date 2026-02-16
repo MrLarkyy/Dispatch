6a79c3e20a64b795e0115c10195147bf770f3e9d
MrLarkyy_ <studiotadikl@gmail.com>
2026-02-16 18:56:50 +0100

    Bump version to 26.0.3 and extend `BukkitCtx` with specialized subclasses for global, location, and entity contexts.

--- Paper/src/main/kotlin/gg/aquatic/dispatch/paper/BukkitCtx.kt
+++ Paper/src/main/kotlin/gg/aquatic/dispatch/paper/BukkitCtx.kt
@@ -2,25 +2,54 @@ package gg.aquatic.dispatch.paper
 
 import gg.aquatic.dispatch.BaseCtx
 import org.bukkit.Bukkit
+import org.bukkit.Location
+import org.bukkit.entity.Entity
 import org.bukkit.plugin.Plugin
 import java.util.concurrent.CompletableFuture
 
-class BukkitCtx(
-    val plugin: Plugin
+sealed class BukkitCtx(
+    val plugin: Plugin,
+    check: () -> Boolean,
+    execution: (Runnable) -> Unit
 ) : BaseCtx(
     check = {
-        plugin.isEnabled && !Bukkit.getServer().isPrimaryThread
+        plugin.isEnabled && check()
     },
     execution = { runnable ->
         val future = CompletableFuture<Void>()
-        plugin.server.scheduler.runTask(plugin, Runnable {
+        execution {
             try {
                 runnable.run()
             } finally {
                 future.complete(null)
             }
-        })
+        }
         future.join()
     },
     logger = plugin.logger
-)
+) {
+
+    class Global(plugin: Plugin) : BukkitCtx(
+        plugin,
+        { !Bukkit.isGlobalTickThread() },
+        { runnable ->
+            Bukkit.getGlobalRegionScheduler().run(plugin) { runnable.run() }
+        }
+    )
+
+    class OfLocation(plugin: Plugin, location: Location) : BukkitCtx(
+        plugin,
+        { !Bukkit.getServer().isOwnedByCurrentRegion(location) },
+        { runnable ->
+            Bukkit.getRegionScheduler().run(plugin, location) { runnable.run() }
+        }
+    )
+
+    class OfEntity(plugin: Plugin, entity: Entity) : BukkitCtx(
+        plugin,
+        { !Bukkit.getServer().isOwnedByCurrentRegion(entity) },
+        { runnable ->
+            entity.scheduler.run(plugin, { runnable.run() }, null)
+        }
+    )
+}
